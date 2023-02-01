0c746773d3df340410c39dc9674f4d3ba50cdece
Sergvah <104735864+Sergvah@users.noreply.github.com>
2023-02-01 15:31:16 +0200

    task-8 well done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,8 @@
-// import Contacts from './Contacts/Contacts';
-// import FilterSearch from './FilterSearch/FilterSearch';
-// import ContactsOfList from './ContactsOfList/ContactsOfList';
-// import { getIsLoadingSelector } from 'redux/selectors';
 import RegisterForm from './RegisterForm/RegisterForm';
 import Login from './Login/Login';
 import Home from './Home/Home';
 import Layout from './Layout/Layout';
 import MainPage from './MainPage/MainPage';
-// import RegisterPage from 'first';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
@@ -23,50 +18,28 @@ export const App = () => {
   }, [dispatch]);
   const isRefreshing = useSelector(selectIsRefreshing);
 
-  return (
-    // <div className="container">
-    //   <h1>Phonebook</h1>
-    //   <Contacts />
-    //   <h2>Contacts</h2>
-    //   <FilterSearch />
-    //   {isLoading}
-    //   <ContactsOfList />
-    //   <RegisterForm />
-    //   <Login />
-    // </div>
-
-    isRefreshing ? (
-      'Fetching user data ...'
-    ) : (
-      <div className="container">
-        <Layout />
-        <Routes>
-          <Route
-            path="/"
-            index
-            element={<RestrictedRoute component={Home} />}
-          />
-          <Route
-            path="/contacts"
-            element={<PrivateRoute component={MainPage} redirectTo="/" />}
-          />
-          <Route
-            path="/login"
-            element={
-              <RestrictedRoute component={Login} redirectTo="/contacts" />
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <RestrictedRoute
-                component={RegisterForm}
-                redirectTo="/contacts"
-              />
-            }
-          />
-        </Routes>
-      </div>
-    )
+  return isRefreshing ? (
+    'Fetching user data ...'
+  ) : (
+    <div className="container">
+      <Layout />
+      <Routes>
+        <Route path="/" index element={<Home />} />
+        <Route
+          path="/contacts"
+          element={<PrivateRoute component={MainPage} />}
+        />
+        <Route
+          path="/login"
+          element={<RestrictedRoute component={Login} redirectTo="/contacts" />}
+        />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute component={RegisterForm} redirectTo="/contacts" />
+          }
+        />
+      </Routes>
+    </div>
   );
 };
--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,25 +1,49 @@
-import css from 'components/ContactItem/contactitem.module.css';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from 'redux/operations';
 import { getIsLoadingSelector } from 'redux/selectors';
+import { Button, Box } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import css from 'components/ContactItem/contactitem.module.css';
 
 const ContactItem = ({ item }) => {
   const dispatch = useDispatch();
   const onDeleteCard = id => dispatch(deleteContact(id));
   const isLoading = useSelector(getIsLoadingSelector);
+
+  const theme = createTheme({
+    status: {
+      danger: '#e53e3e',
+    },
+    palette: {
+      primary: {
+        main: '#0971f1',
+        darker: '#053e85',
+      },
+      neutral: {
+        main: '#ee5e2a',
+        contrastText: '#fff',
+      },
+    },
+  });
   return (
     <li className={css.item}>
       <p className={css.text}>{item.name}:</p>
       <p className={css.numb}>{item.number}</p>
-      <button
-        type="button"
-        className={css.btn}
-        onClick={() => onDeleteCard(item.id)}
-        disabled={isLoading}
-      >
-        Delete
-      </button>
+      <ThemeProvider theme={theme}>
+        <Box textAlign="center">
+          <Button
+            variant="contained"
+            type="button"
+            onClick={() => onDeleteCard(item.id)}
+            disabled={isLoading}
+            color="neutral"
+            className={css.btn}
+          >
+            Delete
+          </Button>
+        </Box>
+      </ThemeProvider>
     </li>
   );
 };
